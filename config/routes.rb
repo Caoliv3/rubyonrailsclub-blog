f03e0418381bd0363d0b0811432608179d0ec695
carlos <carlos.oliveira@arke.com>
2024-08-08 18:23:48 -0300

    adicionando autenticacao com devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'welcome/index'
+  devise_for :admins
+  get "welcome/index"
   namespace :administrate do
     resources :articles
   end
